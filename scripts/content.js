a672b5a1e74ba9140123dea26f04333de9f3f42b
IgelIntelligenz <connor_v_d_murphy@hotmail.com>
2025-06-13 21:35:18 +0100

    initial commit with basic functionality

---
+++ scripts/content.js
@@ -0,0 +1,30 @@
+function checkBrands() {
+  const items = Array.from(document.querySelectorAll("div.feed-grid__item"))
+
+  items.forEach((item) => {
+    const brandName = item.querySelector("p[data-testid$='--description-title']")?.textContent
+    console.log(brandName)
+
+    if (brandName === "Shein" || brandName === "Shein Curve" || brandName === "Primark") {
+      item.style.display = "none"
+    }
+  })
+}
+
+checkBrands()
+
+const observer = new MutationObserver(() => {
+  checkBrands()
+})
+
+const observerOptions = {
+  childList: true,
+  subtree: true,
+}
+
+observer.observe(document.body, observerOptions)
+
+setTimeout(() => {
+  observer.disconnect()
+  console.log("observer has been disconnected!")
+}, 5000)
