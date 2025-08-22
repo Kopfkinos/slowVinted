5cc8269ceb1dc5764025f150c6180af26efbb5bb
IgelIntelligenz <connor_v_d_murphy@hotmail.com>
2025-08-22 15:41:28 +0100

    checking what these changes are

--- pop-up/pop-up.js
+++ pop-up/pop-up.js
@@ -1,17 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const enableButton = document.querySelector("#enable-button")
-  enableButton.addEventListener("click", () => {
-    console.log(enableButton.textContent)
-    if (enableButton.textContent === "ENABLE") {
-      enableButton.textContent = "DISABLE"
-      enableButton.classList.add("enabled")
-    } else {
-      enableButton.textContent = "ENABLE"
-      enableButton.classList.remove("enabled")
-    }
-  })
-
   const brandCheckboxes = document.querySelectorAll("input[type=checkbox]")
+  let enabled = false
 
   let brandsToFilter = []
 
@@ -20,13 +9,34 @@ document.addEventListener("DOMContentLoaded", function () {
       brandsToFilter = Array.from(brandCheckboxes)
         .filter((i) => i.checked)
         .map((i) => i.value)
-      console.log(brandsToFilter)
       if (brandsToFilter.includes("Shein")) {
         brandsToFilter.push("Shein Curve")
       }
+      if (enabled) {
+        chrome.runtime.sendMessage({
+          data: brandsToFilter,
+        })
+      }
+    })
+  })
+  const enableButton = document.querySelector("#enable-button")
+  enableButton.addEventListener("click", () => {
+    console.log(enableButton.textContent)
+    if (enableButton.textContent === "ENABLE") {
+      enableButton.textContent = "DISABLE"
+      enableButton.classList.add("enabled")
+      enabled = true
       chrome.runtime.sendMessage({
         data: brandsToFilter,
       })
-    })
+    } else {
+      enableButton.textContent = "ENABLE"
+      enableButton.classList.remove("enabled")
+      enabled = false
+      brandsToFilter = []
+      chrome.runtime.sendMessage({
+        data: brandsToFilter,
+      })
+    }
   })
 })
